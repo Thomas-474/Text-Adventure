6dfc2767ac9eceb228ec7983227635d6e719a4bd
George_Thomas <George_Thomas@student.mahoningctc.com>
2021-05-05 11:26:44 -0400

    reminder comment

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function DisplaySettings(){
     document.getElementById("settingsDisplay").innerHTML = "<iframe src=\"../settings.html\" height=\"200\" width=\"300\" ></iframe>";
 }
 
-
+// look for more ways of making a pop up menu
 
 
 // jQuery:
