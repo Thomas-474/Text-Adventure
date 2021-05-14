90babaa810e958cdb4790f2a80040e7ec3ac47e4
George_Thomas <George_Thomas@student.mahoningctc.com>
2021-05-14 13:56:48 -0400

    Settings Modal Popup

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
-function DisplaySettings(){
-    document.getElementById("settingsDisplay").innerHTML = "<iframe src=\"../settings.html\" height=\"200\" width=\"300\" ></iframe>";
-}
+//#region Modal
+    const openModalButtons = document.querySelectorAll('[data-modal-target]');
+    const closeModalButtons = document.querySelectorAll('[data-close-button]');
+    const overlay = document.getElementById('overlay');
 
-// look for more ways of making a pop up menu
+    openModalButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const modal = document.querySelector(button.dataset.modalTarget);
+            openModal(modal);
+        })
+    })
 
+    overlay.addEventListener('click', () => {
+        const modals = document.querySelectorAll('.modal.active');
+        modals.forEach(modal => {
+            closeModal(modal);
+        })
+    })
 
-// jQuery:
-// $(document).ready(() => {
-// })
+    closeModalButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const modal = button.closest('.modal');
+            closeModal(modal);
+        })
+    })
+
+    function openModal(modal) {
+        if (modal == null) return;
+        modal.classList.add('active');
+        overlay.classList.add('active');
+    }
+
+    function closeModal(modal) {
+        if (modal == null) return;
+        modal.classList.remove('active');
+        overlay.classList.remove('active');
+    }
+//#endregion
